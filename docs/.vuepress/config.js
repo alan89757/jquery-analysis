10d4fdd01b34fb35e775c9b22c86289f20c9f35a
liangrongjun <liangrongjun@saicmobility.com>
2022-02-07 22:35:44 +0800

    类数组转数组

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,14 +7,15 @@ module.exports = {
     themeConfig: {
       displayAllHeaders: true,
       sidebar: [
-          ['/init', 'jQuery初始化'],
-          ['/extend', "extend实现深浅拷贝"],
-          ['/selector', "实现选择器引擎"],
-          ['/each', "实现遍历each"],
-          ['/load-dom', "DOM加载回调"],
+          ['/init', '初始化'],
+          ['/extend', "深浅拷贝"],
+          ['/makeArray', "类数组转数组"],
+          ['/selector', "选择器引擎"],
+          ['/each', "循环遍历"],
+          ['/load-dom', "DOM加载完回调"],
           ['/handle-dom', "操作DOM"],
-          ['/callbacks', "$.Callbacks添加回调列表"],
-          ['/async-callback', "处理异步回调"]
+          ['/callbacks', "管理回调对象"],
+          ['/async-callback', "异步回调"]
       ]
     }
   }
--- docs/jquery.js
+++ docs/jquery.js
@@ -2,12 +2,22 @@
 
 (function(root) {
     // 构造函数
-    var jQuery = function() {
-        return new jQuery.prototype.init();  //1.  jQuery.prototype.init匿名函数的原型要指向jQuery构造函数的原型， jQuery.prototype.init.prototype = jQuery.prototype
+    var jQuery = function(selector, context) {
+        return new jQuery.prototype.init(selector, context);  //1.  jQuery.prototype.init匿名函数的原型要指向jQuery构造函数的原型， jQuery.prototype.init.prototype = jQuery.prototype
     }
     jQuery.prototype = {
-        init: function() {
-            console.log("I'm init working !")
+        init: function(selector, context) {
+            if(!selector) {
+                return this;
+            }
+            if(typeof selector === "string") { 
+                // 字符串，有两种，一种是普通字符串去查找Element元素，一种是html标签字符串，如<div></div>
+                if(selector.charAt(0) === "<" && selector.charAt(selector.length -1) === ">" && selector.length >=3) { // $("<div>")
+                    
+                }
+            }
+
+            // console.log("I'm init working !")
         },
         css: function() {
             console.log("I'm css working !")
@@ -74,6 +84,27 @@
         },
         isPlainObject: function(obj) { // 是否纯粹对象
             return Object.prototype.toString.call(obj) === '[object Object]';
+        },
+        makeArray: function(arr) { // 类数组转数组
+            var result = [];   // 新建个空数组去存类数组数组
+            if(arr && arr.length) {
+                return jQuery.merge(result, arr);   // 把类数组合并到result
+            }
+        },
+        merge: function(arg1, arg2) {
+            var length1 = arg1.length;  // 单markArray它是个空数组，但merge是个公共方法
+            var length2 = arg2.length;
+            var j = 0;  // 类数组和数组公用
+            if(typeof length2 === "number") { // 真正的数组
+                for (; j < length2; j++) {
+                    arg1[length++] = arg2[j];  // 把arg2依次赋值到arg1中
+                }
+            } else {
+                while (arg2[j] !== undefined) { // arg2 = {"0": "alan1", "1": "alan2"}
+                    arg1[length1++]  = arg2[j++];
+                }
+            }
+            return arg1;
         }
     })
 
