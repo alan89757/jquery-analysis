12816f6778be7a3964f0c8558a1610fbefe9fc0c
liangrongjun <liangrongjun@saicmobility.com>
2022-02-07 10:35:05 +0800

    vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    title: 'Jquery专题教程',
+    description: 'Jquery专题教程',
+    base: '/jquery/',
+    port: 8080,
+    themeConfig: {
+      displayAllHeaders: true,
+      sidebar: [
+          ['/init', '初始化'],
+          ['/extend', "extend"],
+          ['/markArray', "markArray"]
+      ]
+    }
+  }
